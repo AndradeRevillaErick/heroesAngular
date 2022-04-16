2bf06684d9b89ee3823235c07b96fdc79734e475
Erick Andrade <erick_andradhe@hotmail.com>
2022-04-15 19:00:57 -0500

    add vista formulario y servicio agregar

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,9 +9,10 @@ import { MaterialModule } from '../material/material.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+
 import { ImagenPipe } from './pipes/imagen.pipe';
 
 
@@ -22,8 +23,8 @@ import { ImagenPipe } from './pipes/imagen.pipe';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent,
     HeroeTarjetaComponent,
+    ListadoComponent,
     ImagenPipe
   ],
   imports: [
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,7 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatGridListModule } from '@angular/material/grid-list'; 
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list'; 
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -24,6 +25,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
